0b69bf43bf8433cdb5e830157d2cf39e92c90afc
DavPlazas <72804930+DavPlazas@users.noreply.github.com>
2021-05-14 21:00:18 -0500

    Análisis estadístico de datos
    Histograma de los muertos por sexo: Hombres y Mujeres
    Grafico de barras del tiempo diagnostico por localidad
    Regresión Lineal: Casos positivos acumulados por días transcurridos

---
+++ Code/Proyecto.R
@@ -0,0 +1,42 @@
+#Muertos por sexo: hombre vs mujer
+hist(ProyectoEdadesMuertos$EDAD_HOMBRE,breaks=10,main="Muertes por sexo",
+     xlab="Edad",ylab="Frecuencia",col="#0080ff",alpha=0.5)
+     
+legend(x = "topright", legend = c("Hombre", "Mujer"), 
+       fill = c("#0080ff", rgb(1,0,0)), title = "Sexo")
+
+
+hist(ProyectoEdadesMuertos$EDAD_MUJER, add=TRUE, breaks=10,main="Muertes por sexo",
+     xlab="Edad",ylab="Frecuencia",col=rgb(1,0,0, alpha=0.5))
+
+#pie(MuertosLocalidad$Muertos_Localidad,labels = MuertosLocalidad)
+barplot(MuertosLocalidad$Muertos_Localidad)
+
+
+
+#Tiempo diagnostico por localidad
+
+barp<-barplot(height=TimeD$Tiempo_promedio_d,name=TimeD$id_localidad,col="#cc99ff")
+text(barp, TimeD$Tiempo_promedio_d -0.2, labels = TimeD$Tiempo_promedio_d)
+grid(nx = NA, ny = NULL, lwd = 1, lty = 1, col = "gray")
+#legend(x = "topleft", legend = c("-1: Sin dato", "1: Usaquen", "2: Engativa",
+                                  #"3: Fontibon","4: Kennedy","5: Suba",
+                                  #"6: Teusaquillo", "7: Chapinero", "8: Cuidad Bolivar",
+                                  #"9: Barrios Unidos","10: Los Martires", "11: La candelaria",
+                                 #"12: Rafael Uribe Uribe","13: Puente Arandano","14: Tunjuelito",
+                                 #"15: Bosa","16: San Cristobal", "17: Sante Fe","18: Antonio Nariño",
+                                 #"19: Usme", "20: Fuera de Bogota", "21: Sumapaz"
+                                  #),
+       #fill = c("#000066"), title = "Id_localidad",cex = 0.7)
+
+#Regresion lineal: 
+#Numero de casos (covid-positivo) acumulados por 
+#dias transcurridos desde el primer caso de covid en Bogota
+
+x<-prediccioncasos2$Dias_trancurridos
+y<-prediccioncasos2$Cumulative_cases
+plot(x,y,main="scatterplot",xlab="Numero de dias transcurridos",ylab="Numero de casos")
+
+mod2=lm(y~x)
+abline(mod2,col="red",lwd=2)
+summary(mod2)
